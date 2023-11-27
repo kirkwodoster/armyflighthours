e10d99568e8683d5977e7eba843a4d1cfa1b5162
kirkwodoster <coreymkirkwood@gmail.com>
2023-11-26 20:41:57 -0800

    Your commit message

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,59 +1,130 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_verify_email/provider/google_sign_in.dart';
-import 'package:firebase_auth_verify_email/widget/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final ValueNotifier<String> userCheckNotifier = ValueNotifier<String>('');
+  String? _selectedValue;
+
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
-    final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+    final userCheck = Provider.of<GoogleSignInProvider>(context).userCheck;
 
-    if (provider.userCheck == 'NewUser') {
-      setState(() {
-        AlertDialogProvider.showAlertDialog(context);
-      });
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Signed In as',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              user.email!,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            //Text(),
-            SizedBox(height: 40),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size.fromHeight(50),
+    // Update userCheckNotifier whenever userCheck changes
+    userCheckNotifier.value = userCheck;
+
+    return ValueListenableBuilder<String>(
+      valueListenable: userCheckNotifier,
+      builder: (context, value, child) {
+        if (value == 'NewUser') {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Select Birth Month'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('Welcome, new user!'),
+                    SizedBox(height: 20),
+                    DropdownButton<String>(
+                      value: _selectedValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedValue = newValue!;
+                        });
+                      },
+                      items: <String>[
+                        'Jan',
+                        'Feb',
+                        'Mar',
+                        'Apr',
+                        'May',
+                        'Jun',
+                        'Jul',
+                        'Aug',
+                        'Sep',
+                        'Oct',
+                        'Nov',
+                        'Dec'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
               ),
-              icon: Icon(Icons.arrow_back, size: 32),
-              label: Text(
-                'Sign Out',
-                style: TextStyle(fontSize: 24),
-              ),
-              onPressed: () => FirebaseAuth.instance.signOut(),
+            );
+          });
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Home'),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Signed In as',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  user.email!,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 40),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.fromHeight(50),
+                  ),
+                  icon: Icon(Icons.arrow_back, size: 32),
+                  label: Text(
+                    'Sign Out',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  onPressed: () => FirebaseAuth.instance.signOut(),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    userCheckNotifier.dispose();
+    super.dispose();
   }
 }
