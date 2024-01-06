a39e96fd7f9b4bf60b026350581c76b1d0ea7073
kirkwodoster <coreymkirkwood@gmail.com>
2024-01-05 21:00:43 -0800

    Update on_board.dart

--- lib/page/on_board.dart
+++ lib/page/on_board.dart
@@ -1,177 +1,120 @@
-import 'package:firebase_auth_verify_email/page/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
-  const OnBoardingScreen({Key? key}) : super(key: key);
-
   @override
-  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+  _OnboardingPageState createState() => _OnboardingPageState();
 }
 
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final PageController _controller = PageController();
-  int currentIndex = 0;
-  bool onLastPage = false;
+class _OnboardingPageState extends State<OnBoardingScreen> {
+  final controller = PageController();
+  bool isLastPage = false;
+  bool isFirstPage = true;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(children: [
-      PageView(
-        controller: _controller,
-        onPageChanged: (index) {
-          setState(() {
-            currentIndex = index;
-            //onLastPage = (index == 2);
-          });
-        },
-        children: const [
-          IntroPage1(),
-          IntroPage2(),
-          IntroPage3(),
-        ],
-      ),
-      Container(
-        alignment: const Alignment(0, 0.75),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: Container(
+        //height: double.infinity,
+        //width: double.infinity,
+        padding: const EdgeInsets.only(bottom: 80),
+        child: PageView(
+          controller: controller,
+          onPageChanged: (index) {
+            setState(() {
+              isLastPage = index == 2;
+              isFirstPage = index == 0;
+            });
+          },
           children: [
-            SmoothPageIndicator(
-              controller: _controller,
-              count: 3,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [RadioExample()],
             ),
-            currentIndex == 0
-                ? ElevatedButton(
-                    onPressed: () {},
-                    child: GestureDetector(
-                        onTap: () => _controller.nextPage(
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeIn),
-                        child: Text(
-                          'Next',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 19,
-                          ),
-                        )),
-                  )
-                : currentIndex == 1
-                    ? Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: GestureDetector(
-                                onTap: () => _controller.previousPage(
-                                    duration: Duration(milliseconds: 300),
-                                    curve: Curves.easeIn),
-                                child: Text(
-                                  'Back',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 19,
-                                  ),
-                                )),
-                          ),
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: GestureDetector(
-                                onTap: () => _controller.nextPage(
-                                    duration: Duration(milliseconds: 300),
-                                    curve: Curves.easeIn),
-                                child: Text(
-                                  'Next',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 19,
-                                  ),
-                                )),
-                          ),
-                        ],
-                      )
-                    : ElevatedButton(
-                        onPressed: () {},
-                        child: GestureDetector(
-                            onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomePage(),
-                                )),
-                            child: Text(
-                              'Submit',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 19,
-                              ),
-                            )),
-                      ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [FloatInputForm()],
+            ),
+            Stack(alignment: Alignment.center, children: [DateInput()])
           ],
         ),
-      )
-    ]));
-  }
-}
-
-class IntroPage1 extends StatelessWidget {
-  const IntroPage1({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        RadioExample(),
-        Container(
-          color: Colors.blue.shade200,
-        ),
-      ],
-    );
-  }
-}
-
-class IntroPage2 extends StatelessWidget {
-  const IntroPage2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple.shade200,
-      child: const Center(
-        child: Text(
-          'Page 2',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
       ),
-    );
-  }
-}
-
-class IntroPage3 extends StatelessWidget {
-  const IntroPage3({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green.shade200,
-      child: const Center(
-        child: Text(
-          'Page 3',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ),
-    );
-  }
+      bottomSheet: isLastPage
+          ? Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    child: const Text('Back'),
+                    onPressed: () => controller.previousPage(
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    ),
+                  ),
+                  TextButton(
+                    child: const Text('Submit'),
+                    onPressed: () {
+                      print(_RadioExampleState()._character.toString());
+                      ;
+                    },
+                  ),
+                ],
+              ),
+            )
+          : isFirstPage
+              ? Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: const Text('Next'),
+                        onPressed: () => controller.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        child: const Text('Back'),
+                        onPressed: () => controller.previousPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        ),
+                      ),
+                      TextButton(
+                        child: const Text('Next'),
+                        onPressed: () => controller.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        ),
+                      ),
+                    ],
+                  ),
+                ));
 }
 
 enum SingingCharacter { Apache, Blackhawk, Chinook }
 
 class RadioExample extends StatefulWidget {
-  const RadioExample({super.key});
-
   @override
-  State<RadioExample> createState() => _RadioExampleState();
+  _RadioExampleState createState() => _RadioExampleState();
 }
 
 class _RadioExampleState extends State<RadioExample> {
@@ -179,46 +122,135 @@ class _RadioExampleState extends State<RadioExample> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+    // double screenWidth = MediaQuery.of(context).size.width;
+    // double screenHeight = MediaQuery.of(context).size.height;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: <Widget>[
-        ListTile(
-          title: const Text('Apache'),
-          leading: Radio<SingingCharacter>(
-            value: SingingCharacter.Apache,
-            groupValue: _character,
-            onChanged: (SingingCharacter? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.home),
+          label: Text('Home'),
         ),
-        ListTile(
-          title: const Text('Blackhawk'),
-          leading: Radio<SingingCharacter>(
-            value: SingingCharacter.Blackhawk,
-            groupValue: _character,
-            onChanged: (SingingCharacter? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.settings),
+          label: Text('Settings'),
         ),
-        ListTile(
-          title: const Text('Chinook'),
-          leading: Radio<SingingCharacter>(
-            value: SingingCharacter.Chinook,
-            groupValue: _character,
-            onChanged: (SingingCharacter? value) {
-              setState(() {
-                _character = value;
-              });
-            },
-          ),
+        OutlinedButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.info),
+          label: Text('Info'),
         ),
       ],
+    );
+  }
+}
+
+class FloatInputForm extends StatefulWidget {
+  @override
+  _FloatInputFormState createState() => _FloatInputFormState();
+}
+
+class _FloatInputFormState extends State<FloatInputForm> {
+  final _firstController = TextEditingController();
+  final _secondController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Form(
+      key: _formKey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            width: screenWidth * 0.25, // 80% of screen width
+            height: screenHeight * 0.25, // 10% of screen height
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Total Hours"),
+              keyboardType: TextInputType.number,
+              controller: _firstController,
+              validator: (value) {
+                if (double.tryParse(value ?? '') == null) {
+                  return 'Please enter a valid float';
+                }
+                return null;
+              },
+            ),
+          ),
+          //SizedBox(height: 20),
+          Container(
+            width: screenWidth * 0.25,
+            height: screenHeight * 0.25,
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Total NVS"),
+              keyboardType: TextInputType.number,
+              controller: _secondController,
+              validator: (value) {
+                if (double.tryParse(value ?? '') == null) {
+                  return 'Please enter a valid float';
+                }
+                return null;
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AutoDatePicker extends StatefulWidget {
+  @override
+  _AutoDatePickerState createState() => _AutoDatePickerState();
+}
+
+class _AutoDatePickerState extends State<AutoDatePicker> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => selectDate());
+  }
+
+  Future<void> selectDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1940, 8),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // Replace with your actual UI
+  }
+}
+
+class DateInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InputDatePickerFormField(
+            fieldLabelText: 'Enter Birthdate',
+            firstDate: DateTime(2015, 8),
+            lastDate: DateTime.now(),
+            autofocus: true,
+          ),
+        ),
+      ),
     );
   }
 }
